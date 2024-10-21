30b557a060c683a68bf3d710229464403e21949d
AlejandraAcuña <173119479+AlejandraAcu@users.noreply.github.com>
2024-10-21 14:29:48 -0500

    movie-app

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection');
 
-const Movie = sequelize.define('modelName', {
+const Movie = sequelize.define('movie', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
